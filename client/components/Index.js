cd31b8b5c7ecf419327d28d49e3c90fa09e1994a
Nicholas Sedlet <nicholas.sedlet@gmail.com>
2017-12-03 10:53:32 -0500

    formatting & prefix for currency inputs

--- client/components/Index.js
+++ client/components/Index.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import { updateInputField } from '../actions/input'
-import { Button, Input } from 'semantic-ui-react'
+import { Button, Input, Label } from 'semantic-ui-react'
 
 import Result from './Result'
+
+import NumberFormat from 'react-number-format'
 
 // import 'semantic-ui-css/semantic.min.css'
 
@@ -17,16 +19,32 @@ class Index extends Component {
     return((e) => this.props.dispatch(updateInputField(field,e.target.value)))
   }
   
+  _updateField(field,value) {
+   this.props.dispatch(updateInputField(field,value)) 
+  }
+  
   render() {
     const { input, calculate, dispatch } = this.props
     
     return(
       <div>
-        <h1>GOP Tax Plan Impact Calculator!</h1>
+        <h1>GOP Tax Plan Impact Calculator</h1>
         <table>
           <tr>
             <td><label htmlFor='grossIncome'>Gross Income</label></td>
-            <td><Input type='text' id='grossIncome' value={input.grossIncome} onChange={this._handleInputChange('grossIncome')}/></td>
+            <td>
+              <Input 
+                type='text'
+                id='grossIncome'
+                labelPosition='left'
+              >
+                <Label
+                  basic
+                  content='$'
+                />
+                <NumberFormat value={input.grossIncome} thousandSeparator={true} onValueChange={({value}) => this._updateField('grossIncome',value)}/>
+              </Input>
+            </td>
           </tr>
           <tr>
             <td><label htmlFor='filingStatus'>Filing Status</label></td>
@@ -54,19 +72,67 @@ class Index extends Component {
           </tr>
           <tr>
             <td><label htmlFor='mortgageInterest'>Mortgage Interest</label></td>
-            <td><Input type='text' id='mortgageInterest' value={input.mortgageInterest} onChange={this._handleInputChange('mortgageInterest')}/></td>
+            <td>
+              <Input 
+                type='text'
+                id='mortgageInterest'
+                labelPosition='left'
+              >
+                <Label
+                  basic
+                  content='$'
+                />
+                <NumberFormat value={input.mortgageInterest} thousandSeparator={true} onValueChange={({value}) => this._updateField('mortgageInterest',value)}/>
+              </Input>
+            </td>
           </tr>
           <tr>
             <td><label htmlFor='charitableDonations'>Charitable Donations</label></td>
-            <td><Input type='text' id='charitableDonations' value={input.charitableDonations} onChange={this._handleInputChange('charitableDonations')}/></td>
+            <td>
+              <Input 
+                type='text'
+                id='charitableDonations'
+                labelPosition='left'
+              >
+                <Label
+                  basic
+                  content='$'
+                />
+                <NumberFormat value={input.charitableDonations} thousandSeparator={true} onValueChange={({value}) => this._updateField('charitableDonations',value)}/>
+              </Input>
+            </td>
           </tr>
           <tr>
             <td><label htmlFor='stateLocalPropertyTaxes'>State/Local Property Taxes</label></td>
-            <td><Input type='text' id='stateLocalPropertyTaxes' value={input.stateLocalPropertyTaxes} onChange={this._handleInputChange('stateLocalPropertyTaxes')}/></td>
+            <td>
+              <Input 
+                type='text'
+                id='stateLocalPropertyTaxes'
+                labelPosition='left'
+              >
+                <Label
+                  basic
+                  content='$'
+                />
+                <NumberFormat value={input.stateLocalPropertyTaxes} thousandSeparator={true} onValueChange={({value}) => this._updateField('stateLocalPropertyTaxes',value)}/>
+              </Input>
+            </td>
           </tr>
           <tr>
             <td><label htmlFor='stateLocalIncomeTaxes'>State/Local Income Taxes</label></td>
-            <td><Input type='text' id='stateLocalIncomeTaxes' value={input.stateLocalIncomeTaxes} onChange={this._handleInputChange('stateLocalIncomeTaxes')}/></td>
+            <td>
+              <Input 
+                type='text'
+                id='stateLocalIncomeTaxes'
+                labelPosition='left'
+              >
+                <Label
+                  basic
+                  content='$'
+                />
+                <NumberFormat value={input.stateLocalIncomeTaxes} thousandSeparator={true} onValueChange={({value}) => this._updateField('stateLocalIncomeTaxes',value)}/>
+              </Input>
+            </td>
           </tr>
           <tr>
             <td><label htmlFor='personalExemptions'>Personal Exemptions</label></td>
