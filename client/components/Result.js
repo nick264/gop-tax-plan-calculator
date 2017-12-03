6c455b0c6aa8f3b2a5034d73d0a3fbe52050a885
Nicholas Sedlet <nicholas.sedlet@gmail.com>
2017-12-03 18:22:19 -0500

    show the impact formatted

--- client/components/Result.js
+++ client/components/Result.js
@@ -9,10 +9,46 @@ import { calculateFromInputs } from '../utils/calculator'
 
 import { Table } from 'semantic-ui-react'
 
+const FormatImpact = ({value, absOrPct}) => {  
+  let color = null
+  let prefix = ''
+  
+  if(value < 0) {
+    color = '#1ebc30'
+  }
+  else if(value > 0) {
+    color = '#db2828'
+    prefix = '+'
+  }
+  else {
+    color = '#aaa'
+  }
+  
+  return(
+    <strong style={{color: color}}>
+      (
+        {
+          absOrPct ?
+            <NumberFormat thousandSeparator={true} displayType='text' value={value} prefix={prefix + '$'} decimalScale={0}/>
+          :
+            <NumberFormat thousandSeparator={true} displayType='text' value={value * 100} prefix={prefix} suffix='%' fixedDecimalScale={true} decimalScale={1}/>
+        } 
+      )
+    </strong>
+  )
+  
+}
+
 const Result = ({input}) => {
   const resultsCurrent = calculateFromInputs(input,'Current')
   const resultsHouse = calculateFromInputs(input,'House')
   const resultsSenate = calculateFromInputs(input,'Senate')
+  
+  const houseTaxImpact = ( resultsHouse.TotalTax - resultsCurrent.TotalTax )
+  const senateTaxImpact = ( resultsSenate.TotalTax - resultsCurrent.TotalTax )
+  
+  const houseRateImpact = ( resultsHouse.EffectiveTaxRateOnGross - resultsCurrent.EffectiveTaxRateOnGross )
+  const senateRateImpact = ( resultsSenate.EffectiveTaxRateOnGross - resultsCurrent.EffectiveTaxRateOnGross )
 
   return(
     <Table>
@@ -34,9 +70,13 @@ const Result = ({input}) => {
           </Table.Cell>
           <Table.Cell>
             <NumberFormat thousandSeparator={true} displayType='text' value={resultsHouse.TotalTax} prefix='$' decimalScale={0}/>
+            &nbsp;
+            <FormatImpact value={houseTaxImpact} absOrPct={true}/>
           </Table.Cell>
           <Table.Cell>
             <NumberFormat thousandSeparator={true} displayType='text' value={resultsSenate.TotalTax} prefix='$' decimalScale={0}/>
+            &nbsp;
+            <FormatImpact value={senateTaxImpact} absOrPct={true}/>
           </Table.Cell>
         </Table.Row>
         <Table.Row>
@@ -44,13 +84,17 @@ const Result = ({input}) => {
             Effective Rate
           </Table.Cell>
           <Table.Cell>
-            <NumberFormat thousandSeparator={true} displayType='text' value={resultsCurrent.EffectiveTaxRateOnGross * 100} suffix='%' decimalScale={1}/>
+            <NumberFormat thousandSeparator={true} displayType='text' value={resultsCurrent.EffectiveTaxRateOnGross * 100} suffix='%' fixedDecimalScale={true} decimalScale={1}/>
           </Table.Cell>
           <Table.Cell>
-            <NumberFormat thousandSeparator={true} displayType='text' value={resultsHouse.EffectiveTaxRateOnGross * 100} suffix='%' decimalScale={1}/>
+            <NumberFormat thousandSeparator={true} displayType='text' value={resultsHouse.EffectiveTaxRateOnGross * 100} suffix='%' fixedDecimalScale={true} decimalScale={1}/>
+            &nbsp;
+            <FormatImpact value={houseRateImpact} absOrPct={false}/>
           </Table.Cell>
           <Table.Cell>
-            <NumberFormat thousandSeparator={true} displayType='text' value={resultsSenate.EffectiveTaxRateOnGross * 100} suffix='%' decimalScale={1}/>
+            <NumberFormat thousandSeparator={true} displayType='text' value={resultsSenate.EffectiveTaxRateOnGross * 100} suffix='%' fixedDecimalScale={true} decimalScale={1}/>
+            &nbsp;
+            <FormatImpact value={senateRateImpact} absOrPct={false}/>
           </Table.Cell>
         </Table.Row>
       </Table.Body>
