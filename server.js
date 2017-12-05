84c216c46e10129c3cbcad17b6ed37e5369d365e
Nicholas Sedlet <nicholas.sedlet@gmail.com>
2017-12-05 18:32:28 -0500

    roll own force ssl

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 var express = require('express');
-var forceSSL = require('express-force-ssl');
 var app = express();
 const PORT = process.env.PORT || 3000
 
 //path
 var pathToApp = __dirname;
 
+var https_redirect = function(req, res, next) {
+    if (process.env.NODE_ENV === 'production') {
+        if (req.headers['x-forwarded-proto'] != 'https') {
+            return res.redirect('https://' + req.headers.host + req.url);
+        } else {
+            return next();
+        }
+    } else {
+        return next();
+    }
+};
+
+app.use(https_redirect);
+
 app.use('/static', express.static(__dirname + '/public'));
-
-if(process.env.NODE_ENV === 'production') {
-  app.use(forceSSL);  
-}
-
 app.get('/', function(req, res) {
   res.sendFile(pathToApp + '/index.html');
 });
