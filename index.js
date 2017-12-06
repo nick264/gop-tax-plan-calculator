b9b7dd77554ddc4882df20b17fbad9933ea16adb
Nicholas Sedlet <nicholas.sedlet@gmail.com>
2017-12-05 21:32:40 -0500

    get rid of unused react component

--- index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component, PropTypes} from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <div>
-    <h1>Hello world, main index</h1>
-  </div>,
-  document.getElementById('root')
-);
