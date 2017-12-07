02635deea51b3562b24c25444004fa3f26b702c4
Nicholas Sedlet <nicholas.sedlet@gmail.com>
2017-12-06 23:39:49 -0500

    nevermind, this is easy & cheap w/heroku

--- server.js
+++ server.js
@@ -5,20 +5,20 @@ const PORT = process.env.PORT || 3000
 //path
 var pathToApp = __dirname;
 
-// //force ssl
-// var https_redirect = function(req, res, next) {
-//     if (process.env.NODE_ENV === 'production') {
-//         if (req.headers['x-forwarded-proto'] != 'https') {
-//             return res.redirect('https://' + req.headers.host + req.url);
-//         } else {
-//             return next();
-//         }
-//     } else {
-//         return next();
-//     }
-// };
+//force ssl
+var https_redirect = function(req, res, next) {
+    if (process.env.NODE_ENV === 'production') {
+        if (req.headers['x-forwarded-proto'] != 'https') {
+            return res.redirect('https://' + req.headers.host + req.url);
+        } else {
+            return next();
+        }
+    } else {
+        return next();
+    }
+};
 
-// app.use(https_redirect);
+app.use(https_redirect);
 
 // define /static
 app.use('/static', express.static(__dirname + '/public'));
